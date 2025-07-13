da3195698fa8f20148c3624932d5b62f868db550
martin <martinpradoguerra@gmail.com>
2025-07-13 16:19:15 -0500

    Test 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    let count = $state(0);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    Count: {count}
+</button>
